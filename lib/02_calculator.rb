0f446aa1025ddecd5fd5d1d7d71821c2cab978dd
MargVander <margauxvanderbeken@gmail.com>
2019-01-14 13:07:05 +0100

    ajout exo 3

---
+++ lib/02_calculator.rb
@@ -0,0 +1,43 @@
+def add(nb1,nb2)
+	return nb1 + nb2
+end
+
+def subtract(nb1,nb2)
+	return nb1 - nb2
+end
+
+def sum(arr)
+	return arr.sum
+end
+
+def multiply(nb1,nb2)
+	return nb1 * nb2
+end
+
+def power(nb1,nb2)
+	return nb1**nb2
+end
+
+# def factorial(nb)
+# 	result = Array.new
+# 	while nb<= 0
+# 		result << nb
+# 		nb = nb - 1
+# 	end
+# 	puts result.product
+# end
+# puts factorial(3)
+def factorial(nb)
+	result = Array.new
+	if nb == 0
+		return 1
+	elsif nb > 0
+		while nb>= 1
+			result << nb
+			nb = nb - 1
+			end
+			return result.inject(:*)
+		end
+	end
+
+
